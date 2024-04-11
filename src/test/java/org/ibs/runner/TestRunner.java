27726500387cb3a46def0782bc991a16565cf572
AZhizhov <vsreg@hotmail.com>
2024-04-11 22:38:03 +0500

    Поправил pom, теперь плагин работает корректно. Allure отчет строиться.

--- src/test/java/org/ibs/runner/TestRunner.java
+++ src/test/java/org/ibs/runner/TestRunner.java
@@ -11,9 +11,9 @@ import static io.cucumber.core.options.Constants.*;
 @Suite
 @IncludeEngines("cucumber") // объясняем Suite что это cucumber и мы будем запускать feature файл, передаем cucumber
 @ConfigurationParameters({
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
+       // @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
         @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/feature"),
-        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "src/main/java/org/ibs/steps"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.ibs.steps"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
 })
 
